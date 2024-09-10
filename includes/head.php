498442317c2d8bcc9b6d7ceb8ea024a0325328e9
nicolasmastori <nicolasmastori@gmail.com>
2024-09-10 17:32:59 -0300

    Icono Navbar
    Se agregó icono de navbar

--- includes/head.php
+++ includes/head.php
@@ -4,6 +4,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="Assets/css/style.css">
 <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.4.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+<link rel="icon" href="Assets/img/SGD-Invertido-ICO2.ico">
 <!-- Latest compiled and minified CSS -->
 
 <!-- jQuery library -->
