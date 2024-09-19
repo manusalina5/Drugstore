a09141643f203ee3d5edb20c47b4d08ba4b645fb
nicolasmastori <146045451+nicolasmastori@users.noreply.github.com>
2024-09-19 18:08:00 -0300

    tipo documento validaciones

--- View/Paginas/Personas/Documento/editar_tipodocumento.php
+++ View/Paginas/Personas/Documento/editar_tipodocumento.php
@@ -13,19 +13,25 @@ if (isset($_GET['id'])) {
 }
 ?>
 
+<link rel="stylesheet" href="Assets/css/validaciones.css">
+
 <div class="row">
     <div class="col"></div>
     <div class="col">
         <h1>Modificar Tipo de Documento</h1>
-        <form action="Controller/Personas/Documento/tipodocumento.controlador.php" method="POST">
+        <div class="alert alert-danger alert-dismissible fade show p-3 mb-4 no-alerta" role="alert" id="alert">
+        </div>
+        <form id="form" action="Controller/Personas/Documento/tipodocumento.controlador.php" method="POST" onsubmit="return validate(event)">
             <input type="hidden" name="action" value="modificar">
             <input type="hidden" name="idtipodocumento" value="<?php echo $tipodocumentoData['idtipodocumentos']; ?>">
             <div class="mb-3">
                 <label for="valortipodocumento" class="form-label">Valor</label>
-                <input type="text" class="form-control" id="valortipodocumento" name="valortipodocumento" value="<?php echo $tipodocumentoData['valor']; ?>" required>
+                <input type="text" class="form-control" id="valortipodedocumento" name="valortipodocumento" value="<?php echo $tipodocumentoData['valor']; ?>" required>
             </div>
             <button type="submit" class="btn btn-primary">Modificar</button>
         </form>
     </div>
     <div class="col"></div>
 </div>
+
+<script src="Assets/js/Validaciones/alta_tipodocumento.js"></script>
